a96cbf4e0680d08efdcab7394d5edd3c3eb62fe6
YasithSam <yasith.sam7@gmail.com>
2021-09-23 20:56:19 +0530

    new

---
+++ app/src/main/java/com/example/slathletecare/activity/InjuryActivity.java
@@ -0,0 +1,14 @@
+package com.example.slathletecare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InjuryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_injury);
+    }
+}
