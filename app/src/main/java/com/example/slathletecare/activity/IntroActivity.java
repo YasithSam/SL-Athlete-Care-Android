5cc58d9740e721da3676ba578633b392d2145e72
YasithSam <yasith.sam7@gmail.com>
2021-09-23 21:01:47 +0530

    Introduction page

--- app/src/main/java/com/example/slathletecare/activity/IntroActivity.java
+++ app/src/main/java/com/example/slathletecare/activity/IntroActivity.java
@@ -3,11 +3,15 @@ package com.example.slathletecare.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.developer.kalert.KAlertDialog;
 import com.example.slathletecare.R;
+
+
 
 public class IntroActivity extends AppCompatActivity {
     private Button btnLog;
@@ -16,13 +20,33 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        btnLog=findViewById(R.id.btnReg);
-        btnReg=findViewById(R.id.btnLogin);
+        btnReg=findViewById(R.id.btnSignUp);
+        btnLog=findViewById(R.id.btnLogin);
+        KAlertDialog pDialog = new KAlertDialog(this,KAlertDialog.WARNING_TYPE);
+        pDialog.setTitleText("Here's a message!");
+        pDialog.setContentText("It's pretty, isn't it?");
+        pDialog.setConfirmText("Yes,delete it!");
+        pDialog.setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+            @Override
+            public void onClick(KAlertDialog sDialog) {
+                sDialog.dismissWithAnimation();
+            }
+        });
+        pDialog.show();
+
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(IntroActivity.this,SignUpActivity.class));
                 finish();
+            }
+        });
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,LoginActivity.class));
+                finish();
+
             }
         });
     }
