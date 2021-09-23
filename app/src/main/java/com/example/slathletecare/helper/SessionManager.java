491badc1fd7cef2cc650787becf878883576fec9
YasithSam <yasith.sam7@gmail.com>
2021-09-23 21:02:51 +0530

    Sign up

--- app/src/main/java/com/example/slathletecare/helper/SessionManager.java
+++ app/src/main/java/com/example/slathletecare/helper/SessionManager.java
@@ -1,17 +1,22 @@
 package com.example.slathletecare.helper;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+
+import com.example.slathletecare.activity.LoginActivity;
+
+import java.util.HashMap;
 
 public class SessionManager {
 
     private static String TAG = SessionManager.class.getSimpleName();
+    private static SharedPreferences.Editor editor;
 
     // Shared Preferences
     SharedPreferences pref;
 
-    SharedPreferences.Editor editor;
     Context _context;
 
     // Shared pref mode
@@ -20,7 +25,12 @@ public class SessionManager {
     // Shared preferences file name
     private static final String PREF_NAME = "SLAthleteCareLogin";
 
-    private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
+    private static final String IS_LOGIN = "isLoggedIn";
+
+    public static final String username="";
+
+    public static final String userId="";
+
 
     public SessionManager(Context context) {
         this._context = context;
@@ -28,17 +38,63 @@ public class SessionManager {
         editor = pref.edit();
     }
 
-    public void setLogin(boolean isLoggedIn) {
+    public static void createLoginSession(String id, String name) {
+        editor.putBoolean(IS_LOGIN, true);
+        editor.putString(userId,id);
+        // Storing name in pref
+        editor.putString(username, name);
 
-        editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn);
+
 
         // commit changes
         editor.commit();
-
-        Log.d(TAG, "User login session modified!");
     }
 
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Login Activity
+        Intent i = new Intent(_context, LoginActivity.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        _context.startActivity(i);
+    }
+    /**
+     * Get stored session data
+     * */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        // user name
+        user.put(username, pref.getString(username, null));
+        user.put(userId, pref.getString(userId, null));
+        // return user
+        return user;
+    }
+
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(_context, LoginActivity.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Staring Login Activity
+            _context.startActivity(i);
+        }
+
+    }
     public boolean isLoggedIn(){
-        return pref.getBoolean(KEY_IS_LOGGED_IN, false);
+        return pref.getBoolean(IS_LOGIN, false);
     }
 }
