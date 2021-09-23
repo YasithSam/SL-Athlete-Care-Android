fa06f88f87f616567df22c9b58d30dbe3059adfe
YasithSam <yasith.sam7@gmail.com>
2021-09-23 21:02:40 +0530

    Sign up

--- app/src/main/java/com/example/slathletecare/app/AppConfig.java
+++ app/src/main/java/com/example/slathletecare/app/AppConfig.java
@@ -1,12 +1,28 @@
 package com.example.slathletecare.app;
 
 public class AppConfig {
-    // Server user login url
-    public static String URL_LOGIN = "http://192.168.8.143/SL-athlete-care/new.json";
+
+    //Server base url
+    public static String URL_BASE = "http://192.168.8.143/SL-athlete-care/api/v1";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/SL-athlete-care/register.php";
+    public static String URL_REGISTER = URL_BASE+"/SignUp.php";
 
     //Server OTP Request url
-    public static String OTP_REQUEST = "http://192.168.8.143/SL-athlete-care/new.php";
+    public static String OTP_REQUEST = URL_BASE+"/OTP.php";
+
+    // Server user login url
+    public static String URL_LOGIN= URL_BASE+"/Login.php";
+
+    // Server user forget password url
+    public static String URL_FORGET= URL_BASE+"/changePassword.php";
+
+    // Server chat url
+    public static String URL_CHAT= URL_BASE+"/chat.php";
+
+    // Server data injury
+    public static String URL_INJURYDATA= URL_BASE+"/injuryData.php";
+    // Server data injury
+    public static String URL_INJURY= URL_BASE+"/injury.php";
+
 }
