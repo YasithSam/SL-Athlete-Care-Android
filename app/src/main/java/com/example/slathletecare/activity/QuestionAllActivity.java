448dbf776cef1734a3095db746409d3ad335d8f2
YasithSam <yasith.sam7@gmail.com>
2021-10-04 21:59:50 +0530

    gradle update

---
+++ app/src/main/java/com/example/slathletecare/activity/QuestionAllActivity.java
@@ -0,0 +1,14 @@
+package com.example.slathletecare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_all);
+    }
+}
