08f49ac50c4fec583ef7edc32574c3c6d6277075
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:27:20 +0530

    helpers

--- app/src/main/java/com/example/slathletecare/activity/NoticeOneActivity.java
+++ app/src/main/java/com/example/slathletecare/activity/NoticeOneActivity.java
@@ -12,5 +12,6 @@ public class NoticeOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_one);
+        getSupportActionBar().hide();
     }
 }
