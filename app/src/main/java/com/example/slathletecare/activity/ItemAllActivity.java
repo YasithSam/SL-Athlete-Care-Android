3903a112c5ccd71b9057aeb8429ab1ac42201e28
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:27:32 +0530

    helpers

--- app/src/main/java/com/example/slathletecare/activity/ItemAllActivity.java
+++ app/src/main/java/com/example/slathletecare/activity/ItemAllActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.slathletecare.ArticleAdapter;
 import com.example.slathletecare.R;
@@ -19,6 +22,7 @@ public class ItemAllActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ArticleAdapter mAdapter;
     private List<Articles> aList = new ArrayList<>();
+    TextView artone;
 
 
     @Override
@@ -27,28 +31,72 @@ public class ItemAllActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_all);
         recyclerView = (RecyclerView) findViewById(R.id.rv_1);
         getSupportActionBar().hide();
+        artone=findViewById(R.id.artcone);
+        artone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ItemAllActivity.this,ArticleOneActivity.class));
+            }
+        });
 
         mAdapter = new ArticleAdapter(aList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
-        prepareData();
+
     }
-    private void prepareData() {
-        Articles data = new Articles("How to overcome leg injuries", "There are few things that are very importatnt ....", 23,2);
-        aList.add(data);
+//    private void prepareMovieData() {
+//        Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Shaun the Sheep", "Animation", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Up", "Animation", "2009");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Star Trek", "Science Fiction", "2009");
+//        movieList.add(movie);
+//
+//        movie = new Movie("The LEGO Movie", "Animation", "2014");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Iron Man", "Action & Adventure", "2008");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Aliens", "Science Fiction", "1986");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Chicken Run", "Animation", "2000");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Back to the Future", "Science Fiction", "1985");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
+//        movieList.add(movie);
+//
+//        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+//        movieList.add(movie);
+//
+//        mAdapter.notifyDataSetChanged();
+//    }
 
-        data= new Articles("Best place to buy a knee cap", "My coach advised me to go for health care in rajagiriya ....", 34,1);
-        aList.add(data);
-
-        data= new Articles("Best place to buy a knee cap", "My coach advised me to go for health care in rajagiriya ....", 34,1);
-        aList.add(data);
-
-        data= new Articles("Best place to buy a knee cap", "My coach advised me to go for health care in rajagiriya ....", 34,1);
-        aList.add(data);
-
-        mAdapter.notifyDataSetChanged();
-    }
 
 }
