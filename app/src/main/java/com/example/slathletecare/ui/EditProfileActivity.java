b19feb91df9c621e8f9696d36ab76f26a0ffdab9
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:05:47 +0530

    ui forms

---
+++ app/src/main/java/com/example/slathletecare/ui/EditProfileActivity.java
@@ -0,0 +1,212 @@
+package com.example.slathletecare.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.slathletecare.R;
+import com.example.slathletecare.activity.AddSportActivity;
+import com.example.slathletecare.activity.HealthActivity;
+import com.example.slathletecare.activity.SportActivity;
+import com.example.slathletecare.app.AppConfig;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+
+public class EditProfileActivity extends AppCompatActivity {
+    TextView tv1;
+    DatePickerDialog picker;
+    EditText eText,ef,ea,ec,er;
+    Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        tv1=findViewById(R.id.textViewH2);
+        getSupportActionBar().hide();
+        eText=findViewById(R.id.dob);
+        ef=findViewById(R.id.e_full);
+        ea=findViewById(R.id.e_addr);
+        ec=findViewById(R.id.e_city);
+        er=findViewById(R.id.e_r_p);
+        bt=findViewById(R.id.b5);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditProfileActivity.this, HealthActivity.class));
+            }
+        });
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(EditProfileActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String f=ef.getText().toString();
+                String a=ea.getText().toString();
+                String c=ec.getText().toString();
+                String d=eText.getText().toString();
+                String r=ef.getText().toString();
+
+                new AsynEditProfile().execute(f,a,c,d,r);
+            }
+        });
+
+    }
+
+
+    private class AsynEditProfile extends AsyncTask<String,String,String> {
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected String doInBackground(String ... params) {
+            try {
+                // Enter URL address where your php file resides
+                url = new URL(AppConfig.URL_UPDATE_USER);
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+//                $f=empty($_REQUEST['full_name'])?"":$_REQUEST['full_name'];
+//                $a=empty($_REQUEST['address'])?"":$_REQUEST['address'];
+//                $c=empty($_REQUEST['city'])?"":$_REQUEST['city'];
+//                $d=empty($_REQUEST['dob'])?"":$_REQUEST['dob'];
+//                $r=empty($_REQUEST['r_email'])?"":$_REQUEST['r_email'];
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("id","sl-ac-617e516484ac0")
+                        .appendQueryParameter("full_name",params[0])
+                        .appendQueryParameter("address", params[1])
+                        .appendQueryParameter("city", params[2])
+                        .appendQueryParameter("dob", params[3])
+                        .appendQueryParameter("r_email", params[4]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try{
+                JSONObject obj = new JSONObject(result);
+                if (obj.getString("status").equals("ok")) {
+                    startActivity(new Intent(EditProfileActivity.this, ProfileActivity.class));
+
+                }
+                else if (obj.getString("status").equals("n")) {
+                    Toast.makeText(EditProfileActivity.this, "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_LONG).show();
+
+                }
+                else if (obj.getString("status").equals("e")){
+                    Toast.makeText(EditProfileActivity.this, "No data entered", Toast.LENGTH_LONG).show();
+                }
+
+            }
+            catch (JSONException jsonException){
+
+            }
+
+
+        }
+
+    }
+}
