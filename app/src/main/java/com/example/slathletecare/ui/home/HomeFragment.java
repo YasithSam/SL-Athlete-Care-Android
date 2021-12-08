3924ae2174ecce842208cf21ee9d3a1800e81f94
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:25:46 +0530

    fragments

--- app/src/main/java/com/example/slathletecare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/slathletecare/ui/home/HomeFragment.java
@@ -1,14 +1,17 @@
 package com.example.slathletecare.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -19,6 +22,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.slathletecare.ArticleAdapter;
 import com.example.slathletecare.MessageAdapter;
 import com.example.slathletecare.R;
+import com.example.slathletecare.activity.ItemAllActivity;
+import com.example.slathletecare.activity.NoticeAllActivity;
+import com.example.slathletecare.activity.NoticeOneActivity;
+import com.example.slathletecare.activity.QuestionAllActivity;
 import com.example.slathletecare.databinding.FragmentHomeBinding;
 import com.example.slathletecare.model.Articles;
 import com.example.slathletecare.model.Conversation;
@@ -30,6 +37,8 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    CardView cca,ccc,ccq;
+    Button btn1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,9 +47,32 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        cca=root.findViewById(R.id.cca);
+        ccc=root.findViewById(R.id.ccc);
+        ccq=root.findViewById(R.id.ccaq);
+        btn1=root.findViewById(R.id.btnN);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), NoticeOneActivity.class));
+            }
+        });
+        cca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ItemAllActivity.class));
+            }
+        });
+        ccq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), QuestionAllActivity.class));
+            }
+        });
 
         return root;
     }
+
 
 
 
