d6983c4b3639cb0ea850235283528bd8ab2703d0
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:03:46 +0530

    activity

---
+++ app/src/main/java/com/example/slathletecare/activity/PrivacyActivity.java
@@ -0,0 +1,16 @@
+package com.example.slathletecare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.slathletecare.R;
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+    }
+}
