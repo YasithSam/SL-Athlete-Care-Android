95320d010e0ebc2498a7a46d43efe9f19e8e723f
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:25:59 +0530

    fragments

--- app/src/main/java/com/example/slathletecare/ui/ProfileActivity.java
+++ app/src/main/java/com/example/slathletecare/ui/ProfileActivity.java
@@ -2,13 +2,55 @@ package com.example.slathletecare.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.developer.kalert.KAlertDialog;
+import com.example.slathletecare.ForgetPassword;
+import com.example.slathletecare.R;
+import com.example.slathletecare.activity.InjuryActivity;
+import com.example.slathletecare.activity.LoginActivity;
+import com.example.slathletecare.helper.SessionManager;
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView t1,t2,t3,t4;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        t1=findViewById(R.id.tv_edit);
+        t2=findViewById(R.id.tv_change);
+        t3=findViewById(R.id.tv_lgout);
+        getSupportActionBar().hide();
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,EditProfileActivity.class));
+            }
+        });
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 startActivity(new Intent(ProfileActivity.this, ForgetPassword.class));
+            }
+        });
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sessionManager.logoutUser();
+
+
+
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/slathletecare/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/slathletecare/ui/dashboard/DashboardFragment.java
@@ -1,21 +1,33 @@
 package com.example.slathletecare.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.developer.kalert.KAlertDialog;
 import com.example.slathletecare.R;
+import com.example.slathletecare.activity.CaseStudyActivity;
+import com.example.slathletecare.activity.ForumNewActivity;
+import com.example.slathletecare.activity.InjuryActivity;
+import com.example.slathletecare.activity.ScheduleActivity;
 import com.example.slathletecare.databinding.FragmentDashboardBinding;
 import com.example.slathletecare.helper.SessionManager;
+import com.example.slathletecare.tabbed.FAActivity;
+import com.example.slathletecare.ui.FormDetailsActivity;
+import com.example.slathletecare.ui.MyDetailsActivity;
+import com.example.slathletecare.ui.ProfileActivity;
 
 import java.util.HashMap;
 
@@ -26,6 +38,8 @@ public class DashboardFragment extends Fragment {
     TextView tvName;
     TextView tvSport;
     TextView tvInjury;
+    Button btn1;
+    CardView c,c1,c2,c3,c5,c6;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,18 +54,68 @@ public class DashboardFragment extends Fragment {
         HashMap<String, String> user = sessionManager.getUserDetails();
         View root = binding.getRoot();
         tvName=root.findViewById(R.id.tvName);
-        tvInjury=root.findViewById(R.id.tvInjuryName);
         tvSport=root.findViewById(R.id.tvSport);
-        tvName.setText(user.get(SessionManager.username));
+        tvName.setText(user.get(SessionManager.userId));
+        c=root.findViewById(R.id.cc);
+        c1=root.findViewById(R.id.cc2);
+        c2=root.findViewById(R.id.cc3);
+        c3=root.findViewById(R.id.cc4);
+        c5=root.findViewById(R.id.cc5);
+        c6=root.findViewById(R.id.cc6);
+        btn1=root.findViewById(R.id.btnInjury);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), InjuryActivity.class));
+            }
+        });
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MyDetailsActivity.class));
+            }
+        });
+        c6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), FormDetailsActivity.class));
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(new Intent(getActivity(),CaseStudyActivity.class));
+            }
+        });
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),FAActivity.class));
+            }
+        });
 
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ScheduleActivity.class));
+            }
+        });
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ProfileActivity.class));
+            }
+        });
 
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 
 }
