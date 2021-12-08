d74c384a64dcb444b0b804e97c1ff901cb244c37
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:03:22 +0530

    activity

---
+++ app/src/main/java/com/example/slathletecare/activity/EditSportActivity.java
@@ -0,0 +1,16 @@
+package com.example.slathletecare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.slathletecare.R;
+
+public class EditSportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_sport);
+    }
+}
