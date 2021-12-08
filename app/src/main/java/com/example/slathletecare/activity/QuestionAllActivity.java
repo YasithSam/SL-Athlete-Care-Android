08bcff93d970fd461e34bc9940fd9cc67147b0cd
YasithSam <yasith.sam7@gmail.com>
2021-12-08 13:27:10 +0530

    helpers

--- app/src/main/java/com/example/slathletecare/activity/QuestionAllActivity.java
+++ app/src/main/java/com/example/slathletecare/activity/QuestionAllActivity.java
@@ -2,15 +2,26 @@ package com.example.slathletecare.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.slathletecare.R;
 
 public class QuestionAllActivity extends AppCompatActivity {
-
+    TextView id1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_all);
+        id1=findViewById(R.id.qid);
+        getSupportActionBar().hide();
+        id1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuestionAllActivity.this,QuestionOneActivity.class));
+            }
+        });
     }
 }
