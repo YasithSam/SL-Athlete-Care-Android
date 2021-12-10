29fea399643d3a8f8363a324bc77f7c3e2582e99
YasithSam <yasith.sam7@gmail.com>
2021-12-10 10:52:13 +0530

    add

--- app/src/main/java/com/example/slathletecare/casestudy/PageAdapterCS.java
+++ app/src/main/java/com/example/slathletecare/casestudy/PageAdapterCS.java
@@ -1,5 +1,7 @@
 package com.example.slathletecare.casestudy;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -7,21 +9,31 @@ import androidx.fragment.app.FragmentPagerAdapter;
 
 public class PageAdapterCS  extends FragmentPagerAdapter {
     private int numOfTabs;
+    private int Id;
 
-    PageAdapterCS(FragmentManager fm, int numOfTabs) {
+    PageAdapterCS(FragmentManager fm, int numOfTabs,int Id) {
         super(fm);
         this.numOfTabs = numOfTabs;
+        this.Id=Id;
     }
 
     @Override
     public Fragment getItem(int position) {
+        Bundle bundle=new Bundle();
+        bundle.putInt("id",Id);
         switch (position) {
             case 0:
-                return new UpdatesFragment();
+                Fragment fragment=new UpdatesFragment();
+                fragment.setArguments(bundle);
+                return fragment;
             case 1:
-                return new PreFragment();
+                Fragment fragment2=new PreFragment();
+                fragment2.setArguments(bundle);
+                return fragment2;
             case 2:
-                return new PostFragment();
+                Fragment fragment3=new PostFragment();
+                fragment3.setArguments(bundle);
+                return fragment3;
 
             default:
                 return null;
