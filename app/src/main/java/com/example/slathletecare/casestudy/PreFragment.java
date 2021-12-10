514f3c820fc5aaf15554c49dc5ad83c7aaa40fbc
YasithSam <yasith.sam7@gmail.com>
2021-12-10 10:52:24 +0530

    add

--- app/src/main/java/com/example/slathletecare/casestudy/PreFragment.java
+++ app/src/main/java/com/example/slathletecare/casestudy/PreFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.slathletecare.R;
+import com.example.slathletecare.activity.CaseStudyActivity;
 import com.example.slathletecare.casestudy.inner.DietActivity;
 import com.example.slathletecare.casestudy.inner.ImageActivity;
 import com.example.slathletecare.casestudy.inner.MedicineActivity;
@@ -23,40 +24,53 @@ import com.example.slathletecare.databinding.FragmentPreBinding;
 public class PreFragment extends Fragment {
     CardView c1,c2,c3,c4;
     private FragmentPreBinding binding;
+    Bundle bundle;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         setHasOptionsMenu(true);
-         binding= FragmentPreBinding.inflate(inflater, container, false);
-         View v=binding.getRoot();
+        binding= FragmentPreBinding.inflate(inflater, container, false);
+        View v=binding.getRoot();
         c1=v.findViewById(R.id.m);
         c2=v.findViewById(R.id.a);
         c3=v.findViewById(R.id.d1);
         c4=v.findViewById(R.id.d3);
+        bundle=getArguments();
 
         c1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), MedicineActivity.class));
+                Intent myIntent = new Intent(getActivity(), MedicineActivity.class);
+                myIntent.putExtra("id", bundle.getInt("id"));
+                startActivity(myIntent);
+
             }
         });
         c2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), ImageActivity.class));
+                Intent myIntent = new Intent(getActivity(), ImageActivity.class);
+                myIntent.putExtra("id", bundle.getInt("id"));
+                startActivity(myIntent);
+
             }
         });
         c3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), DietActivity.class));
+                Intent myIntent = new Intent(getActivity(), MedicineActivity.class);
+                myIntent.putExtra("id", bundle.getInt("id"));
+                startActivity(myIntent);
+
             }
         });
         c4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), WorkoutActivity.class));
+                Intent myIntent = new Intent(getActivity(), WorkoutActivity.class);
+                myIntent.putExtra("id", bundle.getInt("id"));
+                startActivity(myIntent);
             }
         });
         return v;
