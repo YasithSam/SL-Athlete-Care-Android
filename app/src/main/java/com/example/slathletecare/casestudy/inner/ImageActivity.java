efb999b74818d09b27d4fffc9987fe2865f9b61e
YasithSam <yasith.sam7@gmail.com>
2021-12-18 21:16:28 +0530

    add

--- app/src/main/java/com/example/slathletecare/casestudy/inner/ImageActivity.java
+++ app/src/main/java/com/example/slathletecare/casestudy/inner/ImageActivity.java
@@ -1,17 +1,129 @@
 package com.example.slathletecare.casestudy.inner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.slathletecare.R;
+import com.example.slathletecare.app.AppConfig;
+import com.example.slathletecare.app.HttpHandler;
+import com.example.slathletecare.model.Image;
+import com.example.slathletecare.model.Sport;
+import com.example.slathletecare.model.WorkoutEvent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.slathletecare.AppController.TAG;
 
 public class ImageActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private ImageAdapter mAdapter;
+    private List<Image> sList = new ArrayList<>();
+   Button btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
         getSupportActionBar().hide();
+        btn4=findViewById(R.id.button4);
+        recyclerView = (RecyclerView) findViewById(R.id.rv_img);
+        mAdapter = new ImageAdapter(getApplicationContext(),sList);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ImageActivity.this,AddImageActivity.class));
+            }
+        });
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        new ImageActivity.AsyncGetImages().execute();
+
     }
+    private class AsyncGetImages extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            int id=getIntent().getIntExtra("id",0);
+            int state=getIntent().getIntExtra("type",0);
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(AppConfig.URL_CASE_STUDY_IMAGE+"?id="+id+"&&state="+state);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    // Getting JSON Array node
+                    JSONArray data = jsonObj.getJSONArray("data");
+                    if (data != null) {
+                        JSONObject st=data.getJSONObject(0);
+                        Image s = new Image(st.getString("heading"), st.getString("link"));
+                        sList.add(s);
+                        int len = data.length();
+                        for (int i=1;i<len;i++){
+                            JSONObject sp=data.getJSONObject(i);
+                            s=new Image(sp.getString("heading"), sp.getString("link"));
+                            sList.add(s);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            mAdapter.notifyDataSetChanged();
+
+
+        }
+    }
+
+
+
 }
