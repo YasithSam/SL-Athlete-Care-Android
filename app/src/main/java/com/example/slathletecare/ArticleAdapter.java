26cdac7fceeb17cc7d37b851eb1ea8b7a298ddab
YasithSam <yasith.sam7@gmail.com>
2021-12-18 21:14:45 +0530

    add

--- app/src/main/java/com/example/slathletecare/ArticleAdapter.java
+++ app/src/main/java/com/example/slathletecare/ArticleAdapter.java
@@ -3,37 +3,49 @@ package com.example.slathletecare;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.slathletecare.model.Article;
 import com.example.slathletecare.model.Articles;
 import com.example.slathletecare.model.Conversation;
+import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHolder>{
-    private List<Articles> aList;
+    private List<Article> aList;
+    private View.OnClickListener mOnItemClickListener;
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView heading;
         public TextView desc;
         public TextView likes;
-        public TextView comments;
-        //public String imgUrl;
+        public TextView d;
+        public ImageView imgUrl;
+        public TextView status;
+
         public MyViewHolder(View itemView) {
             super(itemView);
             heading=(TextView) itemView.findViewById(R.id.tv_header);
             desc=(TextView) itemView.findViewById(R.id.tv_article_des);
             likes=(TextView) itemView.findViewById(R.id.tv_l);
-            comments=(TextView) itemView.findViewById(R.id.tv_c);
-
+            d=(TextView) itemView.findViewById(R.id.tv_d);
+            imgUrl=(ImageView) itemView.findViewById(R.id.imageView1);
+            status=(TextView) itemView.findViewById(R.id.t_status);
+            itemView.setTag(this);
+            itemView.setOnClickListener(mOnItemClickListener);
         }
     }
-    public ArticleAdapter(List<Articles> aList) {
+    public void setOnItemClickListener(View.OnClickListener itemClickListener) {
+        mOnItemClickListener = itemClickListener;
+    }
+    public ArticleAdapter(List<Article> aList) {
         this.aList= aList;
     }
 
@@ -48,12 +60,13 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ArticleAdapter.MyViewHolder holder, int position) {
-        Articles a= aList.get(position);
-        holder.heading.setText(a.getHeader());
-        holder.desc.setText(a.getDesc());
-        holder.likes.setText(Integer.toString(a.getLikes()));
-        holder.comments.setText(Integer.toString(a.getComments()));
+        Article a= aList.get(position);
+        holder.heading.setText(a.getHeading());
+        holder.desc.setText(a.getDescription());
+        holder.likes.setText(a.getLikes());
+        holder.d.setText(a.getDatetime());
 
+        Picasso.get().load(a.getUrl()).into(holder.imgUrl);
     }
 
     @Override
