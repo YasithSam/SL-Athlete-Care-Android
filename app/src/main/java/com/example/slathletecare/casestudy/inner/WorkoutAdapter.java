371bf26dee036af23cdfdd67b825e1c1e9771fbd
YasithSam <yasith.sam7@gmail.com>
2021-12-18 21:16:42 +0530

    add

--- app/src/main/java/com/example/slathletecare/casestudy/inner/WorkoutAdapter.java
+++ app/src/main/java/com/example/slathletecare/casestudy/inner/WorkoutAdapter.java
@@ -3,9 +3,12 @@ package com.example.slathletecare.casestudy.inner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.slathletecare.R;
@@ -24,12 +27,18 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.MyViewHo
         public TextView title;
         public TextView id;
         public TextView d;
+        public ImageButton arrow;
+        public LinearLayout l1;
+        public CardView c1;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.w_schedule_name);
             id = (TextView) itemView.findViewById(R.id.w_id);
             d = (TextView) itemView.findViewById(R.id.w_d);
+            c1= (CardView) itemView.findViewById(R.id.cccx);
+            l1=(LinearLayout) itemView.findViewById(R.id.l1) ;
+            arrow=(ImageButton) itemView.findViewById(R.id.arrow_button);
             itemView.setTag(this);
             itemView.setOnClickListener(mOnItemClickListener);
 
