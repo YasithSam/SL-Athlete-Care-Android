0e632df8e88c7bc525480682e033861b2881eaad
YasithSam <yasith.sam7@gmail.com>
2021-12-27 01:49:23 +0530

    changes

--- app/src/main/java/com/example/slathletecare/activity/NoticeOneActivity.java
+++ app/src/main/java/com/example/slathletecare/activity/NoticeOneActivity.java
@@ -1,17 +1,148 @@
 package com.example.slathletecare.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.slathletecare.ArticleAdapter;
 import com.example.slathletecare.R;
+import com.example.slathletecare.app.AppConfig;
+import com.example.slathletecare.app.HttpHandler;
+import com.example.slathletecare.model.Article;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.slathletecare.AppController.TAG;
 
 public class NoticeOneActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArticleAdapter mAdapter;
+    private List<Article> list = new ArrayList<>();
+    TextView tv1,tv2;
+    ImageView i1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_one);
         getSupportActionBar().hide();
+        recyclerView = (RecyclerView) findViewById(R.id.rv_n_n);
+        i1=findViewById(R.id.imageView9);
+        tv1=findViewById(R.id.textView37);
+        tv2=findViewById(R.id.textView42);
+        new NoticeOneActivity.AsyncGetAll().execute();
+        mAdapter = new ArticleAdapter(list);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(onItemClickListener);
     }
+    private class AsyncGetAll extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(AppConfig.URL_GET_ALL+"?type="+String.valueOf(getIntent().getIntExtra("type",0)));
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    // Getting JSON Array node
+                    JSONArray data = jsonObj.getJSONArray("data");
+                    if (data != null) {
+                        JSONObject st=data.getJSONObject(0);
+                        Article s = new Article(st.getString("id"),st.getString("heading"),st.getString("description"), st.getString("datetime"),st.getString("likes"),st.getString("comments"),st.getString("url"),st.getString("status").equals("1")?st.getString("status"):"");
+                        list.add(s);
+                        int len = data.length();
+                        for (int i=1;i<len;i++){
+                            JSONObject t=data.getJSONObject(i);
+                            s=new Article(t.getString("id"),t.getString("heading"),t.getString("description"), t.getString("datetime"),t.getString("likes"),t.getString("comments"),t.getString("url"),t.getString("status").equals("1")?t.getString("status"):"");
+                            list.add(s);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            mAdapter.notifyDataSetChanged();
+            int p=getIntent().getIntExtra("id",0);
+            if(list.get(p).getUrl().isEmpty()){
+                i1.setVisibility(View.GONE);
+            }
+            else{
+                Picasso.get().load(list.get(p).getUrl()).into(i1);
+            }
+            tv1.setText(list.get(p).getHeading());
+            tv2.setText(list.get(p).getDescription());
+
+        }
+    }
+    private View.OnClickListener onItemClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) view.getTag();
+            int position = viewHolder.getAbsoluteAdapterPosition();
+            Article cs = list.get(position);
+            Intent myIntent = new Intent(NoticeOneActivity.this, NoticeOneActivity.class);
+            myIntent.putExtra("type",1);
+            myIntent.putExtra("id",position);
+
+            startActivity(myIntent);
+
+        }
+    };
 }
